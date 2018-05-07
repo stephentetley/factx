b764c30c42e2b1c596a5b131d502d9693c342516
stephentetley <stephen.tetley@gmail.com>
2018-05-07 22:24:50 +0100

    Looking at communicating with SWI Prolog running as a process.

---
+++ src/FactX/SwiProcess.fs
@@ -0,0 +1,28 @@
+﻿module FactX.SwiProcess
+
+open System.Diagnostics
+open System.Text
+
+let runSwiProc (swiPath:string) (args:string) : (int * string) = 
+    let swiInfo = new ProcessStartInfo ()
+    swiInfo.FileName <- swiPath
+    swiInfo.Arguments <- args
+    swiInfo.UseShellExecute <- false
+    swiInfo.RedirectStandardOutput <- true
+
+    let swiProc = new Process()
+    swiProc.EnableRaisingEvents <- true
+
+    let swiOutput = new StringBuilder ()
+    swiProc.OutputDataReceived.AddHandler (
+        DataReceivedEventHandler (
+            fun _ args -> swiOutput.AppendLine(args.Data) |> ignore
+        )
+    )
+    swiProc.StartInfo <- swiInfo
+    swiProc.Start () |> ignore
+    swiProc.BeginOutputReadLine()
+    swiProc.WaitForExit ()
+    (swiProc.ExitCode, swiOutput.ToString() )
+
+
---
+++ src/TestSwi.fsx
@@ -0,0 +1,20 @@
+﻿open System.Diagnostics
+open System.Text
+
+#load "FactX\SwiProcess.fs"
+open FactX.SwiProcess
+
+
+// swi-prolog: halt. to quit
+
+let swiPath = @"C:\Program Files\swipl\bin\swipl.exe"
+
+
+let temp01 () = 
+    Process.Start(swiPath, "").WaitForExit()
+
+let temp02 () = 
+    runSwiProc swiPath ""
+
+
+
