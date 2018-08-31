a592af32633fe46eb3abe5efd3257c7d0fc30872
stephentetley <stephen.tetley@gmail.com>
2018-08-31 11:12:54 +0100

    Added a directory listing parser so we can extract facts about file systems.

---
+++ src/DirectoryFacts.fsx
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Stephen Tetley 2018
+// License: BSD 3 Clause
+
+#I @"..\packages\FParsec.1.0.4-RC3\lib\portable-net45+win8+wp8+wpa81"
+#r "FParsec"
+#r "FParsecCS"
+
+#load "FactX\Extra\DirectoryListing.fs"
+open FactX.Extra.DirectoryListing
+
+
+let getLocalDataFile (fileName:string) : string = 
+    System.IO.Path.Combine(__SOURCE_DIRECTORY__,"../data", fileName)
+
+let test01 () = 
+    let path1 = getLocalDataFile "dir.txt"
+    match readDirRecurseOutput path1 with
+    | Choice1Of2 err -> failwith err
+    | Choice2Of2 ans -> printfn "%s" <| display ans
---
+++ src/FactX/Extra/DirectoryListing.fs
@@ -0,0 +1,241 @@
+﻿// Copyright (c) Stephen Tetley 2018
+// License: BSD 3 Clause
+
+namespace FactX.Extra.DirectoryListing
+
+
+open System
+open System.IO
+open System.Text
+
+open FParsec
+open FParsec.CharParsers
+
+[<AutoOpen>]
+module DirectoryListing = 
+
+
+    type Name = string
+    type FilePath = string
+    type Size = int64
+
+
+    // Mode is not currently interpreted
+    type Properties = 
+        { Mode : string option
+          ModificationTime : DateTime option
+        }
+ 
+
+    type FileObj = 
+        | FsFolder of Name * Properties * FileObj list
+        | FsFile of Name * Properties * Size
+
+    type FileStore = 
+        | FileStore of FilePath * FileObj list
+
+
+    let makeDateTime (year:int) (month:int) (day:int) (hour:int) (minute:int) (second:int) : DateTime = 
+        new DateTime(year, month, day, hour, minute, second)
+
+
+    /// ***** Operations on Files and Folders
+
+
+    let getName (source:FileObj) : string = 
+        match source with
+        | FsFolder(name,_ ,_) -> name
+        | FsFile(name,_,_) -> name
+
+    let isFile (source:FileObj) : bool = 
+        match source with
+        | FsFolder _ -> false
+        | FsFile _ -> true
+
+    let isFolder (source:FileObj) : bool = 
+        match source with
+        | FsFolder _ -> true
+        | FsFile _ -> false
+
+    let tryGetExtension (source:FileObj) : string option = 
+        match source with
+        | FsFolder _ -> None
+        | FsFile (name,_,_) -> Some <| Path.GetExtension name
+
+    let getFiles1 (source:FileStore) : FileObj list = 
+        match source with
+        | FileStore (_,xs) -> List.filter isFile xs
+    
+
+    // *************************************
+    // PARSER
+
+    // Parsing output of "dir" or "dir -Recurse" (Windows)
+
+    // Utility combinators
+    let private ws : Parser<string,unit> = manyChars (pchar ' ' <|> pchar '\t')
+    let private ws1 : Parser<string,unit> = many1Chars (pchar ' ' <|> pchar '\t')
+
+    let private symbol (p:Parser<'a,unit>)      : Parser<'a,unit> = p .>> ws
+    let private symbol1 (p:Parser<'a,unit>)     : Parser<'a,unit> = p .>> ws1
+
+    let private keyword (s:string) : Parser<string,unit> = pstring s .>> ws
+    let private keyword1 (s:string) : Parser<string,unit> = pstring s .>> ws1
+
+    let private lineOf (p:Parser<'a,unit>) : Parser<'a,unit> = 
+        p .>> newline
+
+    let private twice (p:Parser<'a,unit>) : Parser<('a * 'a),unit> = pipe2 p p (fun a b -> (a,b))
+
+    let private blankline : Parser<unit,unit> = lineOf ws >>. preturn ()
+
+    let private pName : Parser<Name,unit> = restOfLine false |>> (fun s -> s.TrimEnd ())
+
+
+    // Note this is UK centric    
+    let private pDateTime : Parser<DateTime,unit> = 
+        pipe5   pint32 
+                (pchar '/' >>. pint32) 
+                (pchar '/' >>. symbol pint32) 
+                pint32 
+                (pchar ':' >>. pint32)
+                (fun dd dm dy th tm -> makeDateTime dy dm dd th tm 0)
+    
+    let private pMode : Parser<string,unit> = many1Chars (lower <|> pchar '-') 
+
+    let private isDir (mode:string) : bool = mode.StartsWith("d")
+
+    // Note - if directory name longer than 100(?) chars it is listed on a new line
+    let pDirPath : Parser<Name,unit> = 
+        let indent = pstring "    "
+        let line1 = pName .>> newline
+        let linesK = indent >>. pName .>> newline
+        pipe2 line1 (many linesK) (fun s ss -> String.concat "" (s::ss))
+
+
+    let private pDirectoryDirective : Parser<Name,unit> = 
+        let indent = manyChars (pchar ' ')
+        indent >>. pstring "Directory:" >>. spaces >>. pDirPath
+
+    let private pHeadings : Parser<string list,unit> = 
+        let columns = pipe4 (keyword "Mode")
+                            (keyword "LastWriteTime")
+                            (keyword "Length")
+                            (keyword "Name")
+                            (fun a b c d -> [a;b;c;d])
+        let underline = restOfLine false
+        columns .>> newline .>> underline
+
+    // Note - file store is flat at parse time (needs postprocessing to build)
+    let private pFileObj : Parser<FileObj,unit> = 
+        let pFolder mode = 
+             pipe2 (symbol pDateTime) 
+                   pName 
+                   (fun timestamp name -> FsFolder (name, { Mode = Some mode; ModificationTime = Some timestamp}, []))
+        let pFile mode = 
+            pipe3 (symbol pDateTime) 
+                  (symbol pint64) 
+                  pName 
+                  (fun timestamp size name-> FsFile (name, { Mode = Some mode; ModificationTime = Some timestamp}, size))
+        let parseK mode = 
+            if isDir mode then pFolder mode else pFile mode
+        (symbol pMode) >>= parseK
+
+
+    type Block = string * FileObj list
+
+
+    let private pBlock : Parser<Block, unit> = 
+        pipe3 (spaces >>. pDirectoryDirective) 
+              (twice blankline >>. lineOf pHeadings >>. many1 (lineOf pFileObj))
+              spaces
+              (fun dirName objs zzz -> (dirName, objs))
+
+
+    let private pListing : Parser<Block list,unit> = many (pBlock .>> spaces)
+
+    // *************************************
+    // Build from flat.
+
+
+    type private Level1Kids = Map<Name, FileObj list>
+
+    // Root is always first
+    let private getRoot (blocks:Block list) : Block option = 
+        match blocks with
+        | x :: _ -> Some x
+        | _ -> None
+
+
+    let private makeLevel1Kids (blocks:Block list) : Level1Kids = 
+        let step acc (dirName, objs) = Map.add dirName objs acc
+        List.fold step Map.empty blocks
+
+    let rec private makeRecur (store:Level1Kids) (parent:Name) (fileObj:FileObj) : FileObj = 
+        match fileObj with
+        | FsFile _ -> fileObj
+        | FsFolder(name,props,_) -> 
+            let fullName = parent + "\\" + name
+            let kids1 = 
+                match Map.tryFind fullName store with
+                | Some(xs) -> xs
+                | None -> []
+
+            let kids2 = List.map (makeRecur store fullName) kids1
+            FsFolder (name,props,kids2) 
+
+
+
+    let private buildTopDown (blocks:Block list) : FileStore option = 
+        match blocks with 
+        | (dirName,objs) :: _ -> 
+            let level1Kids  = makeLevel1Kids blocks
+            let realKids    = List.map (makeRecur level1Kids dirName) objs
+            Some <| FileStore (dirName, realKids)
+        | [] -> None
+
+
+
+    let readDirRecurseOutput (inputPath:string) : Choice<string,FileStore> = 
+        match runParserOnFile pListing () inputPath Text.ASCIIEncoding.ASCII with
+        | Success(a,_,_) ->  
+            match buildTopDown a with
+            | None -> Choice1Of2 "Parsed, but could not build FileStore"
+            | Some ans -> Choice2Of2 ans
+        | Failure(s,_,_) -> Choice1Of2 s
+
+
+    // *************************************
+    // Display
+
+    let private ciCompare (obj1:FileObj) (obj2:FileObj) : int  = 
+        String.Compare(getName obj1, getName obj2, ignoreCase=true)
+
+    let private catPath (str1:string) (str2:string) : string = 
+        if String.IsNullOrEmpty str1 then str2 else str1 + "\\" + str2
+
+    let private display1 (sb:StringBuilder) (source:FileObj) : unit = 
+        let rec work (path:string) (obj1:FileObj)  = 
+            match obj1 with
+            | FsFile (name,_,_) -> sb.AppendLine(catPath path name) |> ignore
+            | FsFolder (name,_,xs) -> 
+                let path1 = catPath path name
+                sb.AppendLine(path1 + "\\") |> ignore
+                let sortedKids = List.sortWith ciCompare xs
+                List.iter (work path1) sortedKids 
+        work "" source
+
+    /// Displays the output as a simple list of folders and files in
+    /// case-insensitive order. The output is patterned after DiffMerge's folder view.
+    let display (source:FileStore) : string = 
+        let sb = new StringBuilder ()
+        
+        match source with
+        | FileStore (path,xs) -> 
+            let sortedKids = List.sortWith ciCompare xs
+            sb.AppendLine(path) |> ignore
+            List.iter (display1 sb) sortedKids
+            sb.ToString ()
+
+
+    
