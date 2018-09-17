2b647b7755c9a577216cf93e1e1ec288e408e326
stephentetley <stephen.tetley@gmail.com>
2018-09-17 12:45:11 +0100

    Work on the ValueReader monad and AssetFacts sample.

--- FactX/FactX/Extra/ValueReader.fs
+++ FactX/FactX/Extra/ValueReader.fs
@@ -18,44 +18,94 @@ module ValueReader =
     let inline private apply1 (ma : ValueReader<'a>) : Option<'a> = 
         match ma with | ValueReader a -> a
 
-    let inline private unitM (x:'a) : ValueReader<'a> = ValueReader (Some x)
+    let inline valReturn (x:'a) : ValueReader<'a> = 
+        ValueReader (Some x)
 
-    let inline private bindM (ma:ValueReader<'a>) (f : 'a -> ValueReader<'b>) : ValueReader<'b> =
+    let inline bindM (ma:ValueReader<'a>) (f : 'a -> ValueReader<'b>) : ValueReader<'b> =
         ValueReader <| 
             match apply1 ma with
             | None -> None
             | Some a -> apply1 (f a)
 
+    let inline valZero () : ValueReader<'a> = 
+        ValueReader <| None
+
+    let inline private altM (ma:ValueReader<'a>) (mb:ValueReader<'a>) : ValueReader<'a> =
+        ValueReader <| 
+            match apply1 ma with
+            | None -> apply1 mb
+            | Some a -> Some a
+
+    let inline private  delayM (fn:unit -> ValueReader<'a>) : ValueReader<'a> = 
+        bindM (valReturn ()) fn 
+    
+    let inline private combineM (ma:ValueReader<unit>) 
+                                (mb:ValueReader<'a>) : ValueReader<'a> = 
+        ValueReader <| 
+            match apply1 ma with
+            | None -> None
+            | Some _ -> apply1 mb
+        
     type ValueReaderBuilder() = 
-        member __.Bind(p,f) = bindM p f
-        member __.Return(x) = unitM x
+        member __.Bind(p,f)         = bindM p f
+        member __.Return(x)         = valReturn x
+        member __.Zero ()           = valZero ()
+        member __.Delay fn          = delayM fn
+        member __.Combine (ma,mb)   = combineM ma mb
 
     let valueReader = new ValueReaderBuilder()
 
     let runValueReader (ma:ValueReader<'a>) : Option<'a> = apply1 ma
 
-    let readString (input:string) : ValueReader<Value> = 
-        ValueReader <|
-            match input with
-            | null -> Some (PString "")
-            | str -> Some (PString str)
+    // Common monadic operations
+    let fmapM (fn:'a -> 'b) (ma:ValueReader<'a>) : ValueReader<'b> = 
+        ValueReader <| 
+           match apply1 ma with
+           | None -> None
+           | Some a -> Some (fn a)
 
-    let readSymbol (input:string) : ValueReader<Value> = 
-        ValueReader <|
+    // This is the nub of embedding FParsec - name clashes.
+    // We avoid them by using longer names in DocSoup.
+
+    /// Operator for fmap.
+    let (|>>) (ma:ValueReader<'a>) (fn:'a -> 'b) : ValueReader<'b> = 
+        fmapM fn ma
+
+    /// Flipped fmap.
+    let (<<|) (fn:'a -> 'b) (ma:ValueReader<'a>) : ValueReader<'b> = 
+        fmapM fn ma
+
+    let (<||>) (ma:ValueReader<'a>) (mb:ValueReader<'a>) : ValueReader<'a> = 
+        altM ma mb
+
+    let readStringRaw (input:string) : ValueReader<string> = 
+       ValueReader <|
             match input with
             | null -> None
-            | str -> Some (PQuotedAtom str)
+            | str -> Some str
 
-    let readDecimal (input:string) : ValueReader<Value> = 
+    let readString (input:string) : ValueReader<Value> = 
+        readStringRaw input |>> PString
+
+    let readSymbol (input:string) : ValueReader<Value> = 
+        readStringRaw input |>> PQuotedAtom
+
+    let readDecimalRaw (input:string) : ValueReader<decimal> = 
         ValueReader <|
             try 
-                let ans = decimal input in Some (PDecimal ans)
+                let ans = decimal input in Some ans
+            with
+            | _ -> None
+
+    let readDecimal (input:string) : ValueReader<Value> = 
+        readDecimalRaw input |>> PDecimal
+
+    let readIntRaw (input:string) : ValueReader<int> = 
+        ValueReader <|
+            try 
+                let ans = int input in Some ans
             with
             | _ -> None
 
     let readInt (input:string) : ValueReader<Value> = 
-        ValueReader <|
-            try 
-                let ans = int input in Some (PInt ans)
-            with
-            | _ -> None
+        readIntRaw input |>> PInt
--- FactXTest/AssetFacts.fsx
+++ FactXTest/AssetFacts.fsx
@@ -23,6 +23,7 @@ open FSharp.Interop.Excel
 open FactX
 open FactX.Extra.ExcelProviderHelper
 open FactX.Extra.ValueReader
+open System.Security.AccessControl
 
 
 let outputFile (filename:string) : string = 
@@ -37,8 +38,6 @@ type UsMiscTable =
 
 type UsMiscRow = UsMiscTable.Row
 
-
-
 let readUsMiscSpreadsheet () : UsMiscRow list = 
     let helper = 
         { new IExcelProviderHelper<UsMiscTable,UsMiscRow>
@@ -48,12 +47,12 @@ let readUsMiscSpreadsheet () : UsMiscRow list =
     excelReadRowsAsList helper (new UsMiscTable())
 
 
-let genSensorDistances () : unit = 
-    let outFile = outputFile "sensor_distances.pl"
+let genSensorFacts () : unit = 
+    let outFile = outputFile "us_sensor_facts.pl"
     
     let distHelper : IFactHelper<UsMiscRow> = 
         { new IFactHelper<UsMiscRow> with
-            member this.Signature = "sensor_distances(pli_code, empty_distance, working_span)."
+            member this.Signature = "us_sensor_distances(pli_code, empty_distance, working_span)."
             member this.ClauseBody (row:UsMiscRow) = 
                 runValueReader <| valueReader { 
                     let! uid        = readSymbol row.Reference
@@ -62,11 +61,163 @@ let genSensorDistances () : unit =
                     return [uid; emptyDist; span]
                     }
         }
-              
-
+             
     let distFacts : FactSet = readUsMiscSpreadsheet () |> makeFactSet distHelper
+    
+    let modelHelper : IFactHelper<UsMiscRow> = 
+        { new IFactHelper<UsMiscRow> with
+            member this.Signature = "us_model(pli_code, manufacturer, model)."
+            member this.ClauseBody (row:UsMiscRow) = 
+                runValueReader <| valueReader { 
+                    let! uid        = readSymbol row.Reference
+                    let! emptyDist  = readString row.Manufacturer
+                    let! span       = readString row.Model
+                    return [uid; emptyDist; span]
+                    }
+        }
+
+    let modelFacts : FactSet = readUsMiscSpreadsheet () |> makeFactSet modelHelper
 
     let pmodule : Module = 
-        new Module("sensor_distances", "sensor_distances.pl", distFacts)
+        new Module( name = "us_sensor_facts"
+                  , db = [modelFacts; distFacts] )
+
+    pmodule.Save(outFile)
+
+
+type Relay13Table = 
+    ExcelFile< @"G:\work\AI2-exports\Ultrasonics_relays_1_3.xlsx",
+               SheetName = "Sheet1!",
+               ForceString = true >
+
+
+type Relay13Row = Relay13Table.Row
+
+let readRelay13Spreadsheet () : Relay13Row list = 
+    let helper = 
+        { new IExcelProviderHelper<Relay13Table,Relay13Row>
+          with member this.ReadTableRows table = table.Data 
+               member this.IsBlankRow row = match row.GetValue(0) with null -> true | _ -> false }
+         
+    excelReadRowsAsList helper (new Relay13Table())
+
+type FixedRelay = 
+    { Uid: string
+      Number: int
+      Function: string }
+
+type ActiveRelay = 
+    { Uid: string
+      Number: int
+      Function: string
+      OnSetpoint: decimal
+      OffSetpoint: decimal }
+
+/// Use ValueReader
+let decodeRelay (uid:string) (number:int) (funName:string) 
+            (ons:string) (offs:string) : Option<Choice<FixedRelay,ActiveRelay>> = 
+    let parseActive = 
+        valueReader {
+            let! uid1    = readStringRaw uid
+            let! name   = readStringRaw funName
+            let! on1     = readDecimalRaw ons
+            let! off1    = readDecimalRaw offs
+            return { Uid = uid1
+                     Number = number
+                     Function = name
+                     OnSetpoint = on1
+                     OffSetpoint = off1 }
+        }
+    let parseFixed = 
+        valueReader {
+            let! uid1    = readStringRaw uid 
+            let! name    = readStringRaw funName  
+            return { Uid = uid1
+                     Number = number
+                     Function = name }
+        }
+    let parser = (parseActive |>> Choice2Of2) <||> (parseFixed |>> Choice1Of2)
+    runValueReader parser
+
+let parseFixed1 (uid:string) (number:int) (funName:string) : ValueReader<FixedRelay> = 
+    valueReader {
+        let! uid1    = readStringRaw uid    
+        return { Uid = uid1
+                 Number = number
+                 Function = funName }
+    }  
+
+
+let parseSilly (uid:string) (number:int) (funName:string) : ValueReader<string> = 
+    valueReader {
+        let! uid1    = readStringRaw uid    
+        return (uid1 + "!!!")
+    }  
+
+let temp () = 
+    let xs = [Choice1Of2 1; Choice2Of2 "name"]
+    List.choose (fun x -> match x with | Choice1Of2 y -> Some y | _ -> None) xs
+
+let temp2 () = 
+    runValueReader <| valueReader { 
+        return ("Raw" + "!!!") 
+        }
+
+// TODO this would be much easier if we could add to FactSets
+let getRelays13 (rows:Relay13Row list) : FixedRelay list * ActiveRelay list = 
+    let cons (opt:Option<'a>) (ac:'a list) : 'a list = 
+        match opt with
+        | None -> ac
+        | Some a -> a::ac
+
+    let rec work ac xs =
+        match xs with
+        | [] -> List.rev ac
+        | (z : Relay13Row) :: zs -> 
+            let r1 = decodeRelay (z.Reference) 1 (z.``Relay 1 Function``) 
+                                 (z.``Relay 1 on Level (m)``) (z.``Relay 1 off Level (m)``)
+            let r2 = decodeRelay (z.Reference) 2 (z.``Relay 2 Function``) 
+                                 (z.``Relay 2 on Level (m)``) (z.``Relay 2 off Level (m)``)    
+            let r3 = decodeRelay (z.Reference) 3 (z.``Relay 3 Function``) 
+                                 (z.``Relay 3 on Level (m)``) (z.``Relay 3 off Level (m)``)    
+            work (cons r3 (cons r1 ac)) zs
+
+    let answers = work [] rows
+    let fixeds = List.choose (fun x -> match x with | Choice1Of2 y -> Some y | _ -> None) answers
+    let actives = List.choose (fun x -> match x with | Choice2Of2 y -> Some y | _ -> None) answers
+    fixeds, actives
+
+let genRelayFacts () : unit = 
+    let outFile = outputFile "us_relay_facts.pl"
+    
+    let fixedRelayHelper : IFactHelper<FixedRelay> = 
+        { new IFactHelper<FixedRelay> with
+            member this.Signature = "us_fixed_relay(pli_code, relay_number, relay_function)."
+            member this.ClauseBody (relay:FixedRelay) = 
+                Some <| [ PQuotedAtom relay.Uid
+                        ; PInt relay.Number
+                        ; PString relay.Function ]
+                    
+        }
+
+    let activeRelayHelper : IFactHelper<ActiveRelay> = 
+        { new IFactHelper<ActiveRelay> with
+            member this.Signature = "us_active_relay(pli_code, manufacturer, model, on_setpoint, off_setpoint)."
+            member this.ClauseBody (relay:ActiveRelay) = 
+                Some <| [ PQuotedAtom relay.Uid
+                        ; PInt relay.Number
+                        ; PString relay.Function 
+                        ; PDecimal relay.OnSetpoint
+                        ; PDecimal relay.OffSetpoint ]
+        }
+     
+    let fixeds1, actives1 = readRelay13Spreadsheet () |> getRelays13
+
+    let fixedFacts : FactSet = fixeds1 |> makeFactSet fixedRelayHelper
+    let activeFacts : FactSet = actives1 |> makeFactSet activeRelayHelper
+
+    let pmodule : Module = 
+        new Module( name = "us_relay_facts"
+                  , db = [fixedFacts; activeFacts] )
 
     pmodule.Save(outFile)
