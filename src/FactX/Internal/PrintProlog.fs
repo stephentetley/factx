60c154d1a2e391a3814debe74c5a038f53a82336
stephentetley <stephen.tetley@gmail.com>
2018-10-14 22:05:38 +0100

    Added placeholder for a new pretty printer. Now we the syntax allows functors we probably need a better printer than Internal.FormatCombinators.

---
+++ src/FactX/Internal/PrintProlog.fs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Stephen Tetley 2018
+// License: BSD 3 Clause
+
+namespace FactX.Internal
+
+open YC.PrettyPrinter.Pretty
+open YC.PrettyPrinter.Doc
+open YC.PrettyPrinter.StructuredFormat
+
+[<AutoOpen>]
+module PrintProlog = 
+
+    let private escapeSpecial (source:string) : string = 
+        source.Replace("\\" , "\\\\")
+
+
+    let simpleAtom (value:string) : Doc = wordL value
+
+    // This must escape.
+    let quotedAtom (value:string) : Doc = wordL <| sprintf "'%s'" (escapeSpecial value)
+
+    let prologString (value:string) : Doc = 
+        wordL <| sprintf "\"%s\"" (escapeSpecial value)
+
+    let prologChar (value:char) : Doc =  wordL <| sprintf "0'%c" value
---
+++ test/Test02.fsx
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Stephen Tetley 2018
+// License: BSD 3 Clause
+
+#I @"..\packages\YC.PrettyPrinter.0.0.5\lib\net40"
+#r "YC.PrettyPrinter"
+open YC.PrettyPrinter.Pretty
+open YC.PrettyPrinter.Doc
+
+#load "..\src\FactX\Internal\PrintProlog.fs"
+open FactX.Internal.PrintProlog
+
+
+let runTest (doc:Doc) : unit = printfn "%s" <| print 80 doc
+
+let test01 () = 
+    let d1:Doc = simpleAtom "tree" in runTest d1
+
