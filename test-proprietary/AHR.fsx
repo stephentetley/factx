72716a680737a3d2b067771290573a430307a1fd
stephentetley <stephen.tetley@gmail.com>
2019-05-30 15:16:19 +0100

    Work on the proprietary AHR example.

--- test-proprietary/AHR.fsx
+++ test-proprietary/AHR.fsx
@@ -36,7 +36,7 @@ open FactX.Extra.ExcelProviderHelper
 // ********** DATA SETUP **********
 
 type MappingTable = 
-    ExcelFile< @"G:\work\Projects\asset_sync\Mapping_Rules.xlsx",
+    ExcelFile< @"G:\work\Projects\asset_sync\AI2_FLOC_Asset_Hierarchy_Rules_V3_FRAGMENT.xlsx",
                 SheetName = "Mapping!",
                 ForceString = true >
 
@@ -75,72 +75,133 @@ let codeMappingSkeleton (table:MappingTable) : ModuleSkeleton =
       PredicateSkeletons = [ codePredicate ]
     }
 
-let descrMapping (row:MappingRow) : Predicate = 
-    predicate "descr_mapping" 
-                [ quotedAtom <| checkInput row.InstAssetTypeCode
-                ; quotedAtom <| checkInput row.PrcgAssetTypeDescription
-                ; quotedAtom <| checkInput row.PrcAssetTypeDescription
-                ; quotedAtom <| checkInput row.``Function (L2 FLOC Description)``
-                ; quotedAtom <| checkInput row.``Process Group (L3 FLOC Description)``
-                ; quotedAtom <| checkInput row.``Process (L4 FLOC Description)``
-                ]
-
-let descrMappingSkeleton (table:MappingTable) : ModuleSkeleton = 
-    let descrPredicate = 
-        { PredicateName = "descr_mapping/6"
-          Comment = "descr_mapping(inst_type:atom, group:atom, process:atom, function_descr:atom, group_descr:atom, process_descr:atom)."
-          WriteFacts = excelProviderWriteFacts excelHelper (Some << descrMapping) table
-        }
-    { OutputPath = @"G:\work\Projects\asset_sync\output\descr_mapping.pl"
-      ModuleName = "descr_mapping"
-      PredicateSkeletons = [ descrPredicate ]
-    }
-
-let main () = 
-    let source = new MappingTable()
-    generateModule (codeMappingSkeleton source)
-    generateModule (descrMappingSkeleton source)
+/// Allowed processes and process groups
 
 let prcg (name:string) : Predicate = 
-    predicate "process_group" 
+    predicate "allowed_process_group" 
                 [ quotedAtom <| checkInput name
                 ]
 
 let prc (name:string) : Predicate = 
-    predicate "process" 
+    predicate "allowed_process" 
                 [ quotedAtom <| checkInput name
                 ]
 
 
 
 let processesSkeleton (table:MappingTable) : ModuleSkeleton = 
-    let prcgPredicate = 
+    let prcgSkeleton = 
         let elements : string list = 
             excelReadRowsAsList excelHelper table
                 |> List.map (fun (row:MappingRow) -> row.PrcgAssetTypeDescription)
                 |> List.sort
                 |> List.distinct
-        { PredicateName = "process_group/1"
-          Comment = "process_group(process_group_name:atom)."
+        { PredicateName = "allowed_process_group/1"
+          Comment = "allowed_process_group(name:atom)."
           WriteFacts = seqWriteFacts (Some << prcg) elements
         }
 
-    let prcPredicate = 
+    let prcSkeleton = 
         let elements : string list = 
             excelReadRowsAsList excelHelper table
                 |> List.map (fun (row:MappingRow) -> row.PrcAssetTypeDescription)
                 |> List.sort
                 |> List.distinct
-        { PredicateName = "process/1"
-          Comment = "process(process_name:atom)."
+        { PredicateName = "allowed_process/1"
+          Comment = "allowed_process(name:atom)."
           WriteFacts = seqWriteFacts (Some << prc) elements
         }
-    { OutputPath = @"G:\work\Projects\asset_sync\output\processes.pl"
-      ModuleName = "processes"
-      PredicateSkeletons = [ prcgPredicate; prcPredicate ]
+    { OutputPath = @"G:\work\Projects\asset_sync\output\process_rules.pl"
+      ModuleName = "process_rules"
+      PredicateSkeletons = [ prcgSkeleton; prcSkeleton ]
+    }
+
+/// level2: code -> description
+
+let descrLookupPredicate (predicateName:string) 
+                         (code:string) 
+                         (descr:string) : Predicate option = 
+    match code, descr with
+    | null, _ -> None
+    | _, null -> None
+    | _,_ -> 
+        Some <| predicate predicateName
+                    [ quotedAtom <| checkInput code
+                    ; quotedAtom <| checkInput descr
+                    ]
+
+
+
+
+
+let level2DescrSkeleton (table:MappingTable) : PredicateSkeleton =
+    let makePred (code:string, functionName:string) : Predicate option = 
+        descrLookupPredicate "level2_function_description"  code functionName
+
+    let getPair (row:MappingRow) : string * string = 
+        (row.``L2 FLOC Code/Object Code``, row.``Function (L2 FLOC Description)``)
+    let elements : (string * string) list = 
+        excelReadRowsAsList excelHelper table
+            |> List.map getPair
+            |> List.sort
+            |> List.distinctBy (fun (a,b) -> a+b)
+    { PredicateName = "level2_function_description/2"
+      Comment = "level2_function_description(code:atom, decription:atom)."
+      WriteFacts = seqWriteFacts makePred elements
+    }
+
+/// level3: code -> description
+
+
+let level3DescrSkeleton (table:MappingTable) : PredicateSkeleton =
+
+    let makePred (code:string, procgName:string) : Predicate option = 
+        descrLookupPredicate "level3_process_group_description" code procgName
+
+    let getPair (row:MappingRow) : string * string = 
+        (row.``L3 FLOC Code/Object Code``, row.``Process Group (L3 FLOC Description)``)
+    let elements : (string * string) list = 
+        excelReadRowsAsList excelHelper table
+            |> List.map getPair
+            |> List.sort
+            |> List.distinctBy (fun (a,b) -> a+b)
+    { PredicateName = "level3_process_group_description/2"
+      Comment = "level3_process_group_description(code:atom, decription:atom)."
+      WriteFacts = seqWriteFacts makePred elements
     }
 
 
-let genProcesses () = 
+/// level4: code -> description
+
+
+
+let level4DescrSkeleton (table:MappingTable) : PredicateSkeleton =
+    let makePred (code:string, procgName:string) : Predicate option = 
+        descrLookupPredicate "level4_process_description" code procgName
+
+    let getPair (row:MappingRow) : string * string = 
+        (row.``L4 FLOC Code/Object Code``, row.``Process (L4 FLOC Description)``)
+    let elements : (string * string) list = 
+        excelReadRowsAsList excelHelper table
+            |> List.map getPair
+            |> List.sort
+            |> List.distinctBy (fun (a,b) -> a+b)
+    { PredicateName = "level4_process_description/2"
+      Comment = "level4_process_description(code:atom, decription:atom)."
+      WriteFacts = seqWriteFacts makePred elements
+    }
+
+let descriptionLookupsSkeleton (table:MappingTable) = 
+    { OutputPath = @"G:\work\Projects\asset_sync\output\description_lookups.pl"
+      ModuleName = "description_lookups"
+      PredicateSkeletons = 
+        [ level2DescrSkeleton table 
+        ; level3DescrSkeleton table
+        ; level4DescrSkeleton table
+        ]
+    }
+let main () = 
     let source = new MappingTable()
+    generateModule (codeMappingSkeleton source)
     generateModule (processesSkeleton source)
+    generateModule (descriptionLookupsSkeleton source)
