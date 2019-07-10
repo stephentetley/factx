1efa03335745b8564e4f14d8cfea44524c3c3a47
stephentetley <stephen.tetley@gmail.com>
2019-07-10 16:48:57 +0100

    Added another proprietary example.

--- test-proprietary/Edm2.fsx
+++ test-proprietary/Edm2.fsx
@@ -8,6 +8,12 @@ open System.Text.RegularExpressions
 #I @"C:\Users\stephen\.nuget\packages\slformat\1.0.2-alpha-20190322\lib\netstandard2.0"
 #r "SLFormat"
 
+#I @"C:\Users\stephen\.nuget\packages\rewriteyourstrings\1.0.0-alpha-20190628\lib\netstandard2.0"
+#r "RewriteYourStrings"
+open RewriteYourStrings.RewriteMonad
+open RewriteYourStrings.Query
+open RewriteYourStrings.Transform
+
 #I @"C:\Users\stephen\.nuget\packages\ExcelProvider\1.0.1\lib\netstandard2.0"
 #r "ExcelProvider.Runtime.dll"
 
@@ -30,6 +36,45 @@ open FactX.Extra.Skeletons
 open FactX.Extra.ExcelProviderHelper
 
 
+
+// ********** Extras for rewrite-your-strings **********
+
+let execRewrite (action : Rewrite)
+                (input : string) : StringRewriter<string> = 
+    rewrite { 
+        let! start = getInput ()
+        do! setInput input
+        let! _ = action
+        let! ans = getInput ()
+        do! setInput start
+        return ans
+    }
+
+let runOptional (ma : StringRewriter<'a>) 
+                (input : string) : string option = 
+    match runRewrite ma input with
+    | Error _ -> None
+    | Ok ans -> Some ans
+
+let primitiveRewrite (operation: RegexOptions -> string -> string option) : Rewrite =
+    rewrite { 
+        let! source = getInput ()
+        let! regexOpts = askOptions ()
+        match operation regexOpts source with
+        | None -> return! rewriteError "primitiveRewrite"
+        | Some ans -> return! setInput ans
+    }
+
+
+let namedRegexMatch (pattern : string) (name : string) : Rewrite = 
+    primitiveRewrite <| fun opts input -> 
+        let rmatch = Regex.Match(input = input, pattern = pattern)
+        if rmatch.Success then 
+            rmatch.Groups.Item(name).Value |> Some
+        else
+            None
+
+
 // ********** DATA SETUP **********
 
 
@@ -48,38 +93,35 @@ let docTableHelper : IExcelProviderHelper<EdmsDocTable, EdmsDocRow> =
 
 
 
-let tryReplace (name :string) (pattern : string) : string option = 
-    let swap1 (str:string) = "_" + str.Substring(1)
-    let rmatch = Regex.Match(name, pattern)
-    if rmatch.Success then
-        let suffix = swap1 rmatch.Value
-        Regex.Replace(name, rmatch.Value, suffix) |> Some
-    else
-        None
 
-let addUnderscore (name : string) : string = 
-    match tryReplace name " NO [0-9] [A-Z]{3}$" with
-    | Some ans -> ans
-    | None -> 
-        match tryReplace name " [0-9] [A-Z]{3}$" with
-        | Some ans -> ans
-        | None -> 
-            match tryReplace name " [A-Z]{3}$" with
-            | Some ans -> ans
-            | None -> name
 
-            
+let addUnderscore : Rewrite = 
+    let rewriteRhs (pattern : string) = 
+        rewrite { 
+            let! rhs1 = matchValue pattern
+            let! rhs2 = execRewrite (replacePrefix "_") rhs1
+            return! modify (fun str -> str.Replace(oldValue = rhs1, newValue = rhs2))
+        }
+    choice  [ rewriteRhs " NO [0-9] [A-Z]{3}$" 
+            ; rewriteRhs " [0-9] [A-Z]{3}$"
+            ; rewriteRhs " [A-Z]{3}$" 
+            ]
 
+
+
+    
 let edmsExtractName (title : string) : string option = 
-    let pattern = "^(?<name>.*?) T0975"
-    let rmatch = Regex.Match(input = title, pattern = pattern)
-    if rmatch.Success then 
-        rmatch.Groups.Item("name").Value |> addUnderscore |> Some
-    else
-        None
+    let extractName  : Rewrite = 
+        rewrite { 
+            do! namedRegexMatch "^(?<name>.*?) T0975" "name" 
+            do! addUnderscore
+            return ()
+        }
+    runOptional extractName title
+
 
 let documentFact (siteName : string) : Predicate = 
-    predicate "document" [ stringTerm siteName ]
+    predicate "edms_document" [ stringTerm siteName ]
 
 let docNames () : string list = 
     let table = new EdmsDocTable ()
@@ -91,3 +133,6 @@ let main () =
     let docs = docNames ()
     runFactWriter 160 outPath 
         <| mapMz (tellPredicate << documentFact) docs 
+
+
+
---
+++ test-proprietary/EquipmentLP.fsx
@@ -0,0 +1,75 @@
+﻿// Copyright (c) Stephen Tetley 2019
+
+#r "netstandard"
+#r "System.Xml.Linq.dll"
+open System.IO
+
+
+
+#I @"C:\Users\stephen\.nuget\packages\FSharp.Data\3.0.1\lib\netstandard2.0"
+#r @"FSharp.Data.dll"
+open FSharp.Data
+
+
+#I @"C:\Users\stephen\.nuget\packages\slformat\1.0.2-alpha-20190322\lib\netstandard2.0"
+#r "SLFormat"
+
+
+
+#load "..\src\FactX\Internal\Common.fs"
+#load "..\src\FactX\Syntax.fs"
+#load "..\src\FactX\Pretty.fs"
+#load "..\src\FactX\FactOutput.fs"
+#load "..\src\FactX\FactWriter.fs"
+#load "..\src-extra\FactX\Extra\Skeletons.fs"
+#load "..\src-extra\FactX\Extra\ExcelProviderHelper.fs"
+open FactX
+open FactX.FactWriter
+open FactX.Extra.Skeletons
+open FactX.Extra.ExcelProviderHelper
+
+
+
+// ********** DATA SETUP **********
+
+
+type EquipmentTable = 
+    CsvProvider< @"G:\work\Projects\asset_sync\equipment_migration_s1.csv">
+
+type EquipmentRow = EquipmentTable.Row
+
+type Equipment = 
+    { AibDescription : string
+      ObjectType : string
+      ObjectClass : string
+    }
+
+let conv1 (row : EquipmentRow) : Equipment = 
+    { AibDescription = row.``Asset Type Description``
+      ObjectType = row.``Object Type``
+      ObjectClass = row.Class
+     }
+
+
+let equipmentFact (item : Equipment) : Predicate = 
+    predicate "equip" 
+        [ stringTerm item.AibDescription
+        ; stringTerm item.ObjectType 
+        ; stringTerm item.ObjectClass
+        ]
+
+let docNames () : Equipment list = 
+    let table = new EquipmentTable ()
+    table.Rows
+        |> Seq.map conv1
+        |> Seq.distinct
+        |> Seq.toList
+
+let main () = 
+    let outPath = @"G:\work\Projects\asset_sync\output\equipment.lp"
+    let docs = docNames ()
+    runFactWriter 160 outPath 
+        <| mapMz (tellPredicate << equipmentFact) docs 
+
+
+
